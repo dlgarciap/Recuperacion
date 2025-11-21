13b7221c0920a1d8b3d80248f32a4122b52a6506
dlgarciap <dgarciap28@miumg.edu.gt>
2025-11-20 20:56:40 -0600

    actualizacionn

--- recu/urls.py
+++ recu/urls.py
@@ -17,11 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('usuarios/', include('usuarios.urls')),
-    path('', include('core.urls')),
+    path('', include('core.urls')),  # ← Esto maneja la raíz del sitio
 ]
