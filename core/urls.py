b17c52fd9d7fc1b1a17273bda34f3f071cf699f8
dlgarciap <dgarciap28@miumg.edu.gt>
2025-11-20 20:57:42 -0600

    actualizaciom

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from django.views.generic import RedirectView
 
 app_name = 'core'
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/usuarios/'), name='home'),  # ← Redirige raíz a usuarios
     path('creditos/', views.creditos, name='creditos'),
 ]
